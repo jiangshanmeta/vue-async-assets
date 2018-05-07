bd2a98f3e435df3e3b4134284da0d4cc60794b36
jiangshanmeta <540118044@qq.com>
2018-05-07 20:29:14 +0800

    初始化项目及目录结构

---
+++ example/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        主页蛤
+    </section>
+</template>
+
+<script>
+export default{
+
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import asyncPlugin from "./async-plugin"
+import asyncAssets from "./async-assets"
+
+export default{
+    install(Vue){
+
+    },
+}
